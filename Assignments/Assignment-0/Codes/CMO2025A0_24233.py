0f4062a627d0882c4a604ad84060dc13d60072a4
Ashrith Sagar <ashrithy@iisc.ac.in>
2025-08-11 15:40:07 +0530

    🚑️ A0

--- Assignments/Assignment-0/Codes/CMO2025A0_24233.py
+++ Assignments/Assignment-0/Codes/CMO2025A0_24233.py
@@ -1,3 +1,8 @@
-from oracle_2025A0 import oracle
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("oracle_2025A0"))
+from oracle_2025A0 import oracle  # type: ignore
 
 a, b = oracle(12345, 42)
+print(f"{a=}, {b=}")
